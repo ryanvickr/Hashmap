c0de54bf6cf2460e652665fa5cbdfd53123b64ec
Ryan Vickramasinghe <ryanvickr@gmail.com>
2024-02-10 18:35:31 -0800

    Implement the barebones "Insert" and "Get" function with a fake hashing function.

--- main.cc
+++ main.cc
@@ -6,10 +6,22 @@
 int main() {
     std::cout << "Hello" << std::endl;
 
-    std::function<int(int)> hash_func = [](int key) {
+    std::function<int(const int&)> hash_func = [](const int& key) {
         return 1;
     };
 
     auto map = std::make_unique<HashMap<int, int>>(10, hash_func);
+
+    map->Insert(1, 3);
+
+    auto* result = map->Get(1);
+    if (result == nullptr) {
+        std::cerr << "fail." << std::endl;
+        return 1;
+    } else {
+        std::cout << "Got value: " << *result << std::endl;
+    }
+
+    std::cout << "Done!" << std::endl;
     return 0;
 }
--- src/hashmap.hpp
+++ src/hashmap.hpp
@@ -4,36 +4,79 @@
 #include <functional>
 #include <utility>
 
+namespace {
+void LOG(std::string_view message) {
+   std::cout << "[Info] " << message << std::endl;
+}
+}
+
 template<typename K, typename V>
 class HashMap {
  public:
-   HashMap(int initial_size, std::function<int(K)> hash_function);
+   HashMap(int initial_size, std::function<int(const K&)> hash_function);
 
    // Inserts a value into the hashmap.
    void Insert(K key, V value);
    // Removes a value from the hashmap.
-   void Remove(K key);
+   void Remove(K& key);
    // Gets the value associated with the key, or returns
    // a null pointer.
-   V& Get(K key);
+   V* Get(const K& key);
    // Returns `true` if the key was found in the map, or 
    // false otherwise.
-   bool Contains(K key);
+   bool Contains(K& key);
    // Returns the size of the hashmap.
    int Size();
+
  private:
    // The underlying array.
-   std::vector<std::pair<K, V>> data_;
+   std::vector<std::unique_ptr<std::pair<K, V>>> data_;
    // Hash function
-   std::function<int(K)> hash_function_;
+   std::function<int(const K&)> hash_function_;
 
    int items = 0;
-    
 };
 
 template<typename K, typename V>
 HashMap<K, V>::HashMap(
-   int initial_size, std::function<int(K)> hash_function)
+   int initial_size, std::function<int(const K&)> hash_function)
    : data_(initial_size), hash_function_(hash_function) {}
+
+template<typename K, typename V>
+void HashMap<K, V>::Insert(K key, V value) {
+   int index = hash_function_(key);
+   // TODO: Validate that this index is within bounds first.
+   std::unique_ptr<std::pair<K, V>>& pair = data_[index];
+
+   // Check for a collision
+   if (pair == nullptr) {
+      // No collision, safe to insert.
+      pair->first = key;
+      pair->second = value;
+      return;
+   }
+
+   // TODO: deal with collisions
+}
+
+template<typename K, typename V>
+V* HashMap<K, V>::Get(const K& key) {
+   int index = hash_function_(key);
+   // TODO: Validate that this index is within bounds first.
+   std::unique_ptr<std::pair<K, V>>& pair = data_[index];
+
+   // If this key is empty, return nullptr.
+   // TODO: Add an overload that allows a default value to be passed in.
+   if (pair == nullptr) {
+      return nullptr;
+   }
+
+   if (pair->first != key) {
+      // We don't have a direct match, linear search.
+      return nullptr;
+   } else {
+      return &pair->second;
+   }
+}
 
 #endif  // HASHMAP_H
